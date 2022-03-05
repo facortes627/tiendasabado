1794d01c0a2fdc5d3607709f4db7b95b650ee7a8
Matiiu <mateotaveraramirez@gmail.com>
2022-03-05 13:31:49 -0500

    se pinta con js

--- tiendavirtual/js/llenadoTienda.js
+++ tiendavirtual/js/llenadoTienda.js
@@ -6,26 +6,45 @@ let productos=[
     {foto:'img/img1.jpg',nombre:"Buso", precio:60000, descripcion:""},
     {foto:'img/img1.jpg',nombre:"Buso", precio:60000, descripcion:""},
     {foto:'img/img1.jpg',nombre:"Buso", precio:60000, descripcion:""},
+    {foto:'img/img1.jpg',nombre:"cosa", precio:60000, descripcion:""},
     {foto:'img/img1.jpg',nombre:"Buso", precio:60000, descripcion:""},
     {foto:'img/img1.jpg',nombre:"Buso", precio:60000, descripcion:""},
-    {foto:'img/img1.jpg',nombre:"Buso", precio:60000, descripcion:""},
-    {foto:"../img/img1.jpg",nombre:"Buso", precio:60000, descripcion:""} 
+    {foto:'img/img1.jpg',nombre:"Buso", precio:60000, descripcion:""} 
 ]
 
-//Recorriendo un arreglo con JS
-//Buscar/Seleccionar/Escuchar
+//Creo la referencia al componente padre
+//Creo una variable para almacenar la base sobre la cual voy a pintar
+let fila=document.getElementById("fila")
+
+//Recorrer array
+//forEach
 productos.forEach(function(producto){
-    console.log("HELLO HOW ARE YOU")
+console.log(producto.nombre)
+console.log(producto.precio)
+console.log(producto.foto)
+
+//Receta para pintar con js 
+
+//1. Comience a crear la extructura que necesita
+
+//creo la columna
+let columna=document.createElement("div")
+columna.classList.add("col")
+
+//creo la tarjeta
+let tarjeta=document.createElement("div")
+tarjeta.classList.add("card")
+tarjeta.classList.add("h-100")
+
+//creo la imagen
+let foto=document.createElement("img")
+foto.classList.add("card-img-top")
+foto.src=producto.foto
+
+//2. Ordenar la estructura
+//Padres e hijos
+
+tarjeta.appendChild (foto)
+columna.appendChild (tarjeta)
+fila.appendChild(columna)
 })
-
-//Escuchando clic en el boton 
-let boton=document.getElementById("boton")
-
-//Detectando el primer evento
-boton.addEventListener("click",cambiarFoto)
-
-//Creando una funcion
-function cambiarFoto(){
-    let foto_prueba=document.getElementById("foto_prueba")
-    foto_prueba.src="../img/p10.jpg"
-} 
