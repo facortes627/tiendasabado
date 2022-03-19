bfc73023cfc6af78d3ccb4a365fe2b9d8c2c5844
Matiiu <mateotaveraramirez@gmail.com>
2022-03-19 13:14:08 -0500

    se modifica index y tienda con js

--- tiendavirtual/js/ampliarInfo.js
+++ tiendavirtual/js/ampliarInfo.js
@@ -1,20 +1,26 @@
 export function ampliarInformacion(event){
 
     let producto={}
+    
     if(event.target.classList.contains("btn")){
       
         producto={
-            foto:event.target.parentElement.querySelector("img").src,
-            nombre:event.target.parentElement.querySelector("label").textContent
+            foto:event.target.parentElement.querySelector("img").src,            
+            nombre:event.target.parentElement.querySelector("p").textContent,
+            precio:event.target.parentElement.querySelector("h3").textContent,
+            
 
         }
         console.log(producto)
         
         //Asociando los valores respectivos al nodal
         let foto=document.getElementById("fotoinfo")
-        foto.src=producto.foto
+        foto.src=producto.foto        
         let nombre=document.getElementById("nombreinfo")
         nombre.textContent=producto.nombre
+        let precio=document.getElementById("precioinfo")
+        precio.textContent=producto.precio
+        
     }
 
 }
--- tiendavirtual/js/llenadoTienda.js
+++ tiendavirtual/js/llenadoTienda.js
@@ -2,21 +2,16 @@
 export function pintarTienda(){
 //Arreglo de objetos
 let productos=[
-    {foto:'img/img1.jpg',nombre:"Buso", precio:70000, descripcion:"YEOU Sudadera con capucha de anime japonés"},
-    {foto:'img/img1.jpg',nombre:"Bolsa Anime", precio:35000, descripcion:"Juego de regalo de bolsa de anime que incluye mochila  con cordón, calcomanías, cordón, máscaras, llavero, collar, tarjetas de Lomo, pulseras, soporte para anillo de teléfono, pasadores de botón"},
-    {foto:'img/img1.jpg',nombre:"Morral", precio:60000, descripcion:"Lanrena - Mochila de anime para estudiantes, impermeable, para viajes, adolescentes, portátil, Style A, L"},
-    {foto:'img/img1.jpg',nombre:"Buso", precio:60000, descripcion:""},
-    {foto:'img/img1.jpg',nombre:"Buso", precio:60000, descripcion:""},
-    {foto:'img/img1.jpg',nombre:"Buso", precio:60000, descripcion:""},
-    {foto:'img/img1.jpg',nombre:"cosa", precio:60000, descripcion:""},
-    {foto:'img/img1.jpg',nombre:"Buso", precio:60000, descripcion:""},
-    {foto:'img/img1.jpg',nombre:"Buso", precio:60000, descripcion:""},
-    {foto:'img/img1.jpg',nombre:"Buso", precio:60000, descripcion:""},
-    {foto:'img/img1.jpg',nombre:"Buso", precio:60000, descripcion:""},
-    {foto:'img/img1.jpg',nombre:"Buso", precio:60000, descripcion:""},
-    {foto:'img/img1.jpg',nombre:"Buso", precio:60000, descripcion:""},
-    {foto:'img/img1.jpg',nombre:"Buso", precio:60000, descripcion:""},
-    {foto:'img/img1.jpg',nombre:"Buso", precio:60000, descripcion:""} 
+    {foto:'img/tienda1.webp',nombre:"Fluidbase Retinol + Vit C",marca:"Genové",precio:70000, descripcion:"YEOU Sudadera con capucha de anime japonés"},
+    {foto:'img/tienda2.webp',nombre:"Effaclar Sérum Ultra Concentrado",marca:"La Roche Posay", precio:143000, descripcion:"Juego de regalo de bolsa de anime que incluye mochila  con cordón, calcomanías, cordón, máscaras, llavero, collar, tarjetas de Lomo, pulseras, soporte para anillo de teléfono, pasadores de botón"},
+    {foto:'img/tienda3.webp',nombre:"Hydraskin Face Hidratante",marca:"Pharmaderm", precio:96000, descripcion:"Lanrena - Mochila de anime para estudiantes, impermeable, para viajes, adolescentes, portátil, Style A, L"},
+    {foto:'img/tienda4.webp',nombre:"Pure Vitamin C10", precio:195000,marca:"La Roche Posay", descripcion:""},
+    {foto:'img/tienda5.webp',nombre:"Sesvitamin C Serum Liposomal", marca:"Sesderma", precio:188000, descripcion:""},
+    {foto:'img/tienda6.webp',nombre:"Pigmentbio Foaming Cream", marca:"Bioderma", precio:109000, descripcion:""},,
+    {foto:'img/tienda7.webp',nombre:" Effaclar Agua Micelar Ultra", marca:"La Roche Posay", precio:87000, descripcion:""},
+    {foto:'img/tienda8.webp',nombre:"Hidraderm Leche Limpiadora", marca:"Sesderma", precio:90000, descripcion:""},
+    {foto:'img/tienda9.webp',nombre:"Loción para Pieles Intolerantes", marca:"Avène", precio:105000, descripcion:""},
+    {foto:'img/tienda10.webp',nombre:"Anti-Pigment Dual Serum", marca:"Eucerin", precio:181000, descripcion:""},
 
 ]
 
@@ -45,34 +40,46 @@ columna.classList.add("mt-5")
 let tarjeta=document.createElement("div")
 tarjeta.classList.add("card")
 tarjeta.classList.add("h-100")
-tarjeta.l
+
 
 //creo la imagen
 let foto=document.createElement("img")
 foto.classList.add("card-img-top")
 foto.src=producto.foto
 
+//creo la marca
+let marca=document.createElement("h6")
+marca.innerHTML= producto.marca
+marca.classList.add("align-self-center")
+
+
 //creo el nombre
-let nombre=document.createElement("label")
+let nombre=document.createElement("p")
 nombre.innerHTML= producto.nombre
 nombre.classList.add("align-self-center")
 
+
 //creo precio
-let precio=document.createElement("label")
+let precio=document.createElement("h3")
 precio.innerHTML="$ "+ producto.precio
-precio.classList.add("titulo")
+precio.classList.add("titulo","mt-2","mb-4")
 
 //hadicionar popap cuando de clic en un boton con js
  //crear boton
 let boton=document.createElement("button")
 boton.setAttribute("type","button")
-boton.classList.add("btn","btn-warning","mx-4")
+boton.classList.add("btn","btn-warning","mx-4","mb-5")
 boton.textContent="ver producto"
+
+//creat descripción
+let descripcion=document.createElement("h4")
+descripcion.innerHTML=producto.descripcion
 //2. Ordenar la estructura
 //Padres e hijos
 
 tarjeta.appendChild(foto)
 tarjeta.appendChild(nombre)
+tarjeta.appendChild(marca)
 tarjeta.appendChild(precio)
 tarjeta.appendChild(boton)
 columna.appendChild (tarjeta)
